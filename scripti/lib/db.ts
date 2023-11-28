6f7c3fc1a25a484687295a1c08ca58c8d4ee7cc0
chuguystyr <chuguystyr@gmail.com>
2023-11-28 17:50:18 +0200

    added connection pooling

--- scripti/lib/db.ts
+++ scripti/lib/db.ts
@@ -3,23 +3,27 @@ import mongoose from 'mongoose';
 const DATABASE_URL = process.env.DATABASE_URL;
 
 if (!DATABASE_URL) {
-  throw new Error("Please define the DATABASE_URL environment variable inside .env.local");
+  throw new Error("Please define the DATABASE_URL environment variable");
 }
 
-let cachedDb: typeof mongoose | null = null;
+let cachedDb: null | typeof mongoose = null;
 
 async function connectDB() {
-  if (cachedDb) {
+  if (cachedDb && mongoose.connection.readyState === 1) {
     console.log("Using cached database instance");
     return cachedDb;
   }
+
   try {
-    const dbInstance = await mongoose.connect(DATABASE_URL!);
+    const dbInstance = await mongoose.connect(DATABASE_URL!, {
+      minPoolSize: 10,
+      maxPoolSize: 25,
+    });
     cachedDb = dbInstance;
+    console.log("New database connection established");
     return dbInstance;
   } catch (error) {
     console.error("Database connection failed", error);
-    throw error;
   }
 }
 
