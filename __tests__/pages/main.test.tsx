34955d5f4a69532063d4f7d527579fa92bf8f172
Korniichuk Mykola Mykolayovych <114870996+chuguystyr@users.noreply.github.com>
2024-04-23 16:06:16 +0300

    test: :white_check_mark: add test for main page's component

---
+++ __tests__/pages/main.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect, beforeAll } from "vitest";
+import Main from "app/page";
+
+describe("main page: general structure", () => {
+  beforeAll(() => {
+    render(<Main />);
+  });
+  it("should have a logo and h1 header", () => {
+    const image = screen.getByRole("img");
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    expect(image).not.toBeNull();
+    expect(heading).not.toBeNull();
+  });
+  it("should have login, signup and get started buttons", () => {
+    const links = screen.getAllByRole("link");
+
+    expect(links).not.toBeNull();
+    expect(links).toHaveLength(3);
+    expect(
+      links.every(
+        (link) => link.textContent === "Login" || "Sign Up" || "Get started",
+      ),
+    ).toBeTruthy();
+  });
+});
