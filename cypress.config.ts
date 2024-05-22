ebb0060a16179f239ca8c0272384d6d4905b7d72
Korniichuk Mykola Mykolayovych <114870996+chuguystyr@users.noreply.github.com>
2024-05-22 15:50:15 +0300

    ci: :green_heart: add configured e2e testing to github actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from "cypress"
 
 export default defineConfig({
   e2e: {
+    specPattern: [
+      "cypress/e2e/signup.cy.ts",
+      "cypress/e2e/login.cy.ts",
+      "cypress/e2e/accessibility.cy.ts",
+      "cypress/e2e/courses.cy.ts",
+      "cypress/e2e/tasks.cy.ts",
+      "cypress/e2e/schedule.cy.ts",
+      "cypress/e2e/account.cy.ts",
+    ],
     setupNodeEvents(on, config) {
       const envName = config.env["type-of-environment"] || "local"
       const envType = config.env[envName]
