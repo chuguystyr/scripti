31c88ee9637cc80cdfd868fbfa8439a6e2f3e92b
Korniichuk Mykola Mykolayovych <114870996+chuguystyr@users.noreply.github.com>
2024-06-05 11:20:18 +0300

    fix: :white_check_mark: fix SearchBar unit test

--- __tests__/components/searchBar.test.tsx
+++ __tests__/components/searchBar.test.tsx
@@ -1,14 +1,20 @@
 import { cleanup, render, screen } from "@testing-library/react"
 import SearchBar from "components/SearchBar"
-import { beforeEach, describe, expect } from "vitest"
+import { replace } from "node_modules/cypress/types/lodash"
+import { beforeEach, describe, expect, it, vi } from "vitest"
 
 describe("search bar", () => {
   beforeEach(() => {
     cleanup()
   })
   it("should render search bar", () => {
+    vi.mock("next/navigation", () => ({
+      useSearchParams: vi.fn(),
+      usePathname: vi.fn(),
+      useRouter: vi.fn(() => ({ replace: vi.fn() })),
+    }))
     render(<SearchBar />)
-    const input = screen.getByRole("input", { name: "search" })
+    const input = screen.getByPlaceholderText("Search")
     expect(input).toBeDefined()
   })
 })
