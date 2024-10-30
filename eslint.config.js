ef8b5829243fb20f4fa22856c957edf97cd3e498
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2024-10-30 17:15:51 +0200

    refactor: :sparkles: update ESLint configuration and package scripts for improved compatibility

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,11 @@
-import { fixupConfigRules } from "@eslint/compat"
-import { FlatCompat } from "@eslint/eslintrc"
-import tseslint from "typescript-eslint"
+import { FlatCompat } from '@eslint/eslintrc'
 
-const compat = new FlatCompat()
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
 
-export default [
-  {
-    ignores: [".next/", "node_modules/", ".vercel/", ".vscode/"],
-  },
-  ...tseslint.configs.recommended,
-  ...fixupConfigRules(compat.extends("plugin:@next/next/recommended")),
-  {
-    files: ["**/**/*.js", "**/**/*.ts", "**/**/*.jsx", "**/**/*.tsx"],
-    languageOptions: {
-      parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-  },
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'prettier', 'next/typescript'),
 ]
+
+export default eslintConfig
