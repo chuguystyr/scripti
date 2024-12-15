626916751cb11224667a1cc0536e6e6b42e2b7f5
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2024-12-15 20:30:00 +0200

    fix: :bug: update ESLint configuration to ignore specific directories and files

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
-import { FlatCompat } from '@eslint/eslintrc'
+import { FlatCompat } from "@eslint/eslintrc"
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'prettier', 'next/typescript'),
+  ...compat.extends("next/core-web-vitals", "prettier", "next/typescript"),
+  {
+    ignores: [".husky", ".vercel", ".vscode", "node_modules", "**.js"],
+  },
 ]
 
 export default eslintConfig
