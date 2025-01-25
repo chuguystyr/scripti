ccc009bdb463d091c55a0456659e69ce8d9b69fb
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-01-25 18:40:28 +0200

    refactor: :coffin: remove useless logic in error page

--- app/error.tsx
+++ app/error.tsx
@@ -1,58 +1,31 @@
 "use client"
 import { Metadata } from "next"
-import Link from "next/link"
-export const generateMetadata = async ({
-  error,
-}: {
-  error: Error & { digest?: string }
-}): Promise<Metadata> => {
-  return {
-    title: `${error.message === "Unauthorized" ? "Unauthorized" : "InternalError"} | Scripti`,
+export const metadata: Metadata = {
+  title: "InternalError | Scripti",
+  description: "An error occurred",
+  robots: "noindex, nofollow",
+  openGraph: {
+    title: "Error",
     description: "An error occurred",
-    robots: "noindex, nofollow",
-    openGraph: {
-      title: "Error",
-      description: "An error occurred",
-      type: "website",
-    },
-    twitter: {
-      card: "summary",
-      title: "Error",
-      description: "An error occurred",
-    },
-  }
+    type: "website",
+  },
+  twitter: {
+    card: "summary",
+    title: "Error",
+    description: "An error occurred",
+  },
 }
 const Error: React.FC<{
   error: Error & { digest?: string }
   reset: () => void
-}> = ({ error, reset }) => {
-  const data = { number: 0, text: "" }
-  switch (error.message) {
-    case "Unauthorized":
-      data.number = 401
-      data.text =
-        "You don't have access to this page. Please login to continue."
-      break
-    case "Internal":
-      data.number = 500
-      data.text = "Something went wrong. Please try again later."
-      break
-    default:
-      data.number = 500
-      data.text = "Something went wrong. Please try again later."
-  }
+}> = ({ reset }) => {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-6xl font-bold animate-bounce">{data.number}</h1>
-      <p className="text-xl text-center mt-5 px-2">{data.text}</p>
-      {data.number === 401 ?
-        <Link href="/login" className="mt-5 btn-outlined">
-          Log in
-        </Link>
-      : <button onClick={reset} className="mt-5 btn-outlined">
-          Reload page
-        </button>
-      }
+      <h1 className="text-6xl font-bold animate-bounce">500</h1>
+      <p className="text-xl text-center mt-5 px-2">Internal Error</p>
+      <button onClick={reset} className="mt-5 btn-outlined">
+        Reload page
+      </button>
     </main>
   )
 }
