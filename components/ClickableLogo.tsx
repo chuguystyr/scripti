56d6a58b520fa1b0dc23617da914c310080b1186
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-01-31 15:17:55 +0200

    feat: :sparkles: update ClickableLogo component to accept major prop for dynamic routing

--- components/ClickableLogo.tsx
+++ components/ClickableLogo.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image"
 import Form from "next/form"
 import Logo from "public/Logo.png"
-const ClickableLogo = () => {
+const ClickableLogo: React.FC<{ major: number }> = ({ major }) => {
   return (
-    <Form action="/protected/home/0">
+    <Form action={`/protected/home/${major}`}>
       <button type="submit">
         <Image src={Logo} alt="logo" className="w-50 rounded-2xl" priority />
       </button>
