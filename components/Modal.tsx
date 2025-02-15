0023e466f6df73a89af694a7b4c8f7e9c0f42121
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-02-15 22:38:38 +0200

    fix: :bug: replace Link with button for modal close functionality and use router for navigation

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,17 +1,18 @@
-import Link from "next/link"
+"use client"
 import { FaWindowClose } from "react-icons/fa"
+import { useRouter } from "next/navigation"
 
 const Modal: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const router = useRouter()
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black/50">
       <div className="relative bg-white p-6 mx-2 rounded-lg shadow-lg">
-        <Link
+        <button
           className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-          href="/protected/account"
-          scroll={false}
+          onClick={() => router.back()}
         >
           <FaWindowClose />
-        </Link>
+        </button>
         {children}
       </div>
     </div>
