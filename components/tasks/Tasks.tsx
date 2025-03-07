5d2ef280b4a28165912c573c19d3740cf04fd478
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-03-07 14:17:48 +0200

    fix: :bug: fix setTask form appearing on it's own when user has no tasks to complete

--- components/tasks/Tasks.tsx
+++ components/tasks/Tasks.tsx
@@ -1,7 +1,6 @@
 import { getTasks } from "server/fetchers"
 import TaskCard from "components/tasks/TaskCard"
 import { Params } from "types/Utilities"
-import SetTask from "components/tasks/SetTask"
 import Link from "next/link"
 
 const Tasks: React.FC<Params> = async ({ params }) => {
@@ -11,8 +10,16 @@ const Tasks: React.FC<Params> = async ({ params }) => {
     <section className="flex flex-col md:col-start-3 md:col-end-5 md:row-start-2 md:grid md:grid-cols-2 gap-5">
       {tasks && tasks.length === 0 && (
         <div className="card h-fit block mx-auto text-center col-start-1 col-end-3">
-          <p className="mb-2">Doddy is free!</p>
-          <SetTask task={undefined} />
+          <p className="mb-2">
+            Doddy is free! If you need to add a new task click{" "}
+            <Link
+              href={`/protected/home/${major}/tasks/new`}
+              className="font-semibold hover:underline underline-offset-4"
+              scroll={false}
+            >
+              here
+            </Link>
+          </p>
         </div>
       )}
       {tasks &&
