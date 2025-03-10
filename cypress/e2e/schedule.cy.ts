835a168f4e85aaa67d84999fcc40c30eb6c942e6
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-03-10 16:27:04 +0200

    fix: :bug: update course addition flow to use links instead of buttons; improve date formatting and input names for better clarity

--- cypress/e2e/schedule.cy.ts
+++ cypress/e2e/schedule.cy.ts
@@ -17,7 +17,7 @@ describe("testing schedule functionality", () => {
       cy.login(username, password)
     })
     cy.goToCoursesPage()
-    cy.get("button").contains("Add Course").click()
+    cy.get("a").contains("New Course").click()
     cy.fixture("courses").then(({ validCourse }) => {
       cy.get('input[name="title"]').type(validCourse.title)
       cy.get('input[name="teacherLectures"]').type(validCourse.teacherLectures)
@@ -28,7 +28,7 @@ describe("testing schedule functionality", () => {
       cy.get('input[name="lecturesLink"]').type(validCourse.lecturesLink)
       cy.get('input[name="practicesLink"]').type(validCourse.practicesLink)
       cy.get('input[name="notes"]').type(validCourse.notes)
-      cy.contains("button", /^Add$/).click()
+      cy.contains("button", /^Add Course$/).click()
     })
 
     cy.fixture("users").then(({ correctUser: { username, password } }) => {
@@ -37,22 +37,21 @@ describe("testing schedule functionality", () => {
     cy.get("a").contains("here").click()
 
     const today = new Date()
-    const formattedToday = `${String(today.getDate()).padStart(2, "0")}.${String(today.getMonth() + 1).padStart(2, "0")}.${today.getFullYear()}`
+    const formattedToday = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`
 
     cy.get('input[name="from"]').type(formattedToday)
 
     const nextWeek = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000)
-    const formattedNextWeek = `${String(nextWeek.getDate()).padStart(2, "0")}.${String(nextWeek.getMonth() + 1).padStart(2, "0")}.${nextWeek.getFullYear()}`
-    cy.get('input[name="to"]').type(formattedNextWeek)
+    const formattedNextWeek = `${nextWeek.getFullYear()}-${String(nextWeek.getMonth() + 1).padStart(2, "0")}-${String(nextWeek.getDate()).padStart(2, "0")}`
+    cy.get('input[name="till"]').type(formattedNextWeek)
+    cy.get("input[name='time0']").type("08:00 - 09:30")
 
     cy.fixture("courses").then(({ validCourse }) => {
-      cy.get(`input[name="Monday1"]`).type(validCourse.title)
-      cy.get("li").contains(validCourse.title).click()
-      cy.get(`select`).select("Lecture")
+      cy.get(`select[name="Monday1"]`).select(validCourse.title)
       cy.get(`input[name="room"]`).type("601")
     })
 
-    cy.get("button").contains("Submit").click()
+    cy.get("button").contains("Save").click()
     cy.waitUntil(() => cy.get("h2").contains("Hello,"))
   })
   after(() => {
