c720cddd26d0ef6b9802eeba2b66f41d34320c8a
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-03-25 21:05:08 +0200

    feat: enhance TaskCard component with deadline indicator and vertical dividers

--- components/tasks/TaskCard.tsx
+++ components/tasks/TaskCard.tsx
@@ -3,6 +3,8 @@ import { RiDeleteBin7Fill } from "react-icons/ri"
 import Task from "types/Task"
 import { checkTask, deleteTask } from "server/actions/tasks"
 import EditLink from "components/EditLink"
+import { AiFillFire } from "react-icons/ai"
+import { RxDividerVertical } from "react-icons/rx"
 
 const TaskCard: React.FC<Task> = async ({
   _id,
@@ -12,6 +14,13 @@ const TaskCard: React.FC<Task> = async ({
   deadline,
   description,
 }) => {
+  const date =
+    new Date(deadline) > new Date() ?
+      <AiFillFire className="inline text-amber-500" />
+    : new Date(deadline).toLocaleString("ua-UK", {
+        day: "2-digit",
+        month: "2-digit",
+      })
   return (
     <div className="card flex flex-col gap-2 p-4 h-fit">
       <div className="flex justify-between">
@@ -31,13 +40,10 @@ const TaskCard: React.FC<Task> = async ({
         </form>
       </div>
       <h2>
-        <span className="font-mono">
-          {new Date(deadline).toLocaleString("ua-UK", {
-            day: "2-digit",
-            month: "2-digit",
-          })}
-        </span>{" "}
-        | <span>{course as string}</span> |{" "}
+        <span className="font-mono">{date}</span>
+        <RxDividerVertical className="inline" />
+        <span>{course as string}</span>
+        <RxDividerVertical className="inline" />
         <span className="font-serif">{status}</span>
       </h2>
       <p className="line-clamp-1 md:line-clamp-4">{description}</p>
