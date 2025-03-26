c307926402b640a83c4d66a493c1794d100c8052
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-03-26 21:36:37 +0200

    fix: enhance CreateTaskModal and EditTaskModal to fetch and pass courses to SetTask component

--- app/protected/@modal/(.)home/[major]/tasks/new/page.tsx
+++ app/protected/@modal/(.)home/[major]/tasks/new/page.tsx
@@ -1,10 +1,25 @@
 import Modal from "components/Modal"
 import SetTask from "components/tasks/SetTask"
+import Course from "models/Course"
+import User from "models/User"
+import { protector } from "server/protection"
+import { Params } from "types/Utilities"
+import dbConnect from "server/db"
 
-const CreateTaskModal: React.FC = () => {
+const CreateTaskModal: React.FC<Params> = async ({ params }) => {
+  const { major } = await params
+  const id = await protector()
+  await dbConnect()
+  const { majors } = await User.findById(id, { majors: 1 }).lean().orFail()
+  const courses = await Course.find(
+    { userId: id, major: majors[+major] },
+    { title: 1, _id: { $toString: "$_id" } },
+  )
+    .lean<{ _id: string; title: string }[]>()
+    .orFail()
   return (
     <Modal>
-      <SetTask task={undefined} />
+      <SetTask task={undefined} courses={courses} />
     </Modal>
   )
 }
--- components/tasks/SetTask.tsx
+++ components/tasks/SetTask.tsx
@@ -2,9 +2,11 @@
 import { editTask, setTask } from "server/actions/tasks"
 import Task, { TaskType } from "types/Task"
 import { useActionState } from "react"
-const SetTask: React.FC<{ task: Omit<Task, "userId"> | undefined }> = ({
-  task,
-}) => {
+const SetTask: React.FC<
+  { task: Omit<Task, "userId"> | undefined } & {
+    courses: { _id: string; title: string }[]
+  }
+> = ({ task, courses }) => {
   const [message, formAction, pending] = useActionState(
     task ? editTask : setTask,
     null,
@@ -34,13 +36,20 @@ const SetTask: React.FC<{ task: Omit<Task, "userId"> | undefined }> = ({
       <label htmlFor="course" className="font-semibold">
         Course
       </label>
-      <input
-        type="text"
+      <select
         name="course"
         id="course"
         className="p-2 border border-gray-300 rounded-md"
-        defaultValue={task?.course.toString()}
-      />
+        defaultValue={
+          courses.find((course) => course.title === task?.course)?._id
+        }
+      >
+        {courses.map((course) => (
+          <option key={course._id} value={course._id}>
+            {course.title}
+          </option>
+        ))}
+      </select>
 
       <label htmlFor="type" className="font-semibold">
         Type
