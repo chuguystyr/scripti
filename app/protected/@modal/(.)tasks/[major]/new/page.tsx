c1f56db555a552efd573397d2d7bf33596b176a7
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-03-26 20:44:00 +0200

    fix: rename CreateCourseModal to CreateTaskModal for consistency

--- app/protected/@modal/(.)tasks/[major]/new/page.tsx
+++ app/protected/@modal/(.)tasks/[major]/new/page.tsx
@@ -1,11 +1,11 @@
 import Modal from "components/Modal"
 import SetTask from "components/tasks/SetTask"
 
-const CreateCourseModal: React.FC = () => {
+const CreateTaskModal: React.FC = () => {
   return (
     <Modal>
       <SetTask task={undefined} />
     </Modal>
   )
 }
-export default CreateCourseModal
+export default CreateTaskModal
