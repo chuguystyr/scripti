b6ceb1df75b0ce369b1d9af4edab4377d364fc9c
Korniichuk Mykola Mykolayovych <chuguystyr@gmail.com>
2025-04-03 19:28:14 +0300

    fix: redirect to courses page when no courses are found in SetSchedulePage and add metadata

--- app/protected/setSchedule/[major]/page.tsx
+++ app/protected/setSchedule/[major]/page.tsx
@@ -1,10 +1,19 @@
 import SetSchedule from "components/schedule/SetSchedule"
+import { Metadata } from "next"
+import { redirect } from "next/navigation"
 import { getCourses } from "server/fetchers"
 import { BasicPageProps } from "types/Utilities"
+
+export const metadata: Metadata = {
+  title: "Set schedule | Scripti",
+  description: "Manage your schedule",
+  robots: "noindex, nofollow",
+}
 
 const SetSchedulePage: React.FC<BasicPageProps> = async ({ params }) => {
   const { major } = await params
   const courses = await getCourses(+major)
+  if (courses.length === 0) redirect(`/protected/courses/${major}`)
   return (
     <main className="p-4 max-w-4xl mx-auto">
       <h1 className="sr-only">Set schedule page</h1>
